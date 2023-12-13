df5e1bb14d337c29abe8843c5a5ec58fbbea1b68
jioak <gakhil399@gmail.com>
2023-12-13 17:08:42 +0530

    first commit2

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
   </div>
   <div>
     <Routes>
-      <Route path="/"  element={<Home/>}/>
+      <Route path="/shopApp-Redux"  element={<Home/>}/>
       <Route path="/cart"  element={<Cart/>}/>
     </Routes>
   </div>
